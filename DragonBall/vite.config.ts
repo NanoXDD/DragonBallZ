9944823e85328cd820669c272a48a3558a655308
tatianaHG2 <nanopchg@gmail.com>
2026-04-17 13:50:40 -0400

    .

--- DragonBall/vite.config.ts
+++ DragonBall/vite.config.ts
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],  // ← Arreglado: faltaba una coma y paréntesis
+  publicDir: 'public',  // ya es el valor por defecto
+  root: 'public'
+  
 })
